581a4d61fc08a208eaedea2023efc8ba4a25fa09
vedant koditkar <vedant.koditkar.vk@gmail.com>
2016-11-14 00:07:07 +0530

    Initial Commit

---
+++ src/WebApi2.Web.Api.Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2.Web.Api.Models
+{
+    public class Status
+    {
+        public long StatusId { get; set; }
+        public string Name { get; set; }
+        public int Ordinal { get; set; }
+    }
+}
