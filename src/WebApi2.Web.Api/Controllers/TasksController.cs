ae5e27e022b77179f048ae64e0d1bb23b72a7c53
vedant koditkar <vedant.koditkar.vk@gmail.com>
2016-11-14 00:16:30 +0530

    Added Tasks ApiController

---
+++ src/WebApi2.Web.Api/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2.Web.Api.Controllers
+{
+    public class TasksController : ApiController
+    {
+        [Route("api/tasks/{id:int}")]
+        public string Get(int id)
+        {
+            return "In the Get(int id) overload id = " + id;
+        }
+
+        [Route("api/tasks/{tasknum:alpha}")]
+        public string Get(string taskNum)
+        {
+            return "In the Get(string taskNum) overload taskNum = " + taskNum;
+        }
+    }
+}
